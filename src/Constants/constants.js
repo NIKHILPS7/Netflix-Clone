6f2eae5595e26f95632e87529a01c2eb3d571c1f
NIKHIL PS <shpr046@gmail.com>
2023-03-26 14:40:01 +0530

    add first commit

---
+++ src/Constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3'
+export const API_KEY="d1ad4a8a9730f430f1eb921a32c638e9";
+export const imageUrl='https://image.tmdb.org/t/p/original';
---
+++ src/Constants/urls.js
@@ -0,0 +1,3 @@
+import { API_KEY } from "./constants" 
+export const orginals =`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=28&with_watch_monetization_types=flatrate`
+export const action=`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&without_genres=28&with_watch_monetization_types=flatrate`
